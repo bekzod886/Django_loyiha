d8ac9de5e401025884cc0b9a04b4f6decf94d0b3
Your Name <you@example.com>
2021-08-03 11:59:03 +0300

    djangoda ishlar

--- loyihaapp/models.py
+++ loyihaapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Meva(models.Model):
+    title = models.CharField(max_length=255)
+    img_url = models.CharField(max_length=255)
+    desc = models.CharField(max_length=255)
+    price = models.FloatField()
+
--- loyihaapp/views.py
+++ loyihaapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from . import models
 
 # Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'index.html'
+
+
+home = HomePageView()
+def homeView(request):
+    mevalar = models.Meva.objects.all()
+    return render(request, home.template_name, {'mevalar': mevalar})
