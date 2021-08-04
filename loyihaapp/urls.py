69f3af160c6a59e76c29932e1af1e1ba6e6de77c
Your Name <you@example.com>
2021-08-04 12:19:32 +0300

    django darslar

--- loyihaapp/urls.py
+++ loyihaapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import HomePageView, homeView
+from loyihaapp import views
 
 urlpatterns = [
-    path('', homeView, name='index')
+    path('', homeView, name='index'),
+    path('about/', views.aboutView, name='about'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('login/praktika/',views.praktika, name='praktika'),
 ]
--- loyihaapp/views.py
+++ loyihaapp/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from . import models
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -9,6 +12,46 @@ class HomePageView(TemplateView):
 
 
 home = HomePageView()
+
+
 def homeView(request):
     mevalar = models.Meva.objects.all()
     return render(request, home.template_name, {'mevalar': mevalar})
+
+
+def aboutView(request):
+    return render(request, "about.html")
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        pasword1 = request.POST['password1']
+        pasword2 = request.POST['password2']
+
+        if pasword2 == pasword1:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Bu Email band!')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Bu Username band!')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=pasword1)
+                return redirect('login')
+        else:
+            messages.info(request, 'Parolni tog\'ri takrorlang!')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def praktika(request):
+    email = request.GET['email']
+    password = request.GET['password']
+    return render(request, 'praktika.html', {'email': email, 'password': password})
